6b3ce47c0bfc091a9a6183d148608fd842ac776b
pedroygor <pedroygor2010@hotmail.com>
2022-12-23 15:24:01 -0300

    feat: starting documentation with swagger

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import { router } from './routes';
+import swaggerFile from './swagger.json'; 
+import swaggerUI from 'swagger-ui-express';
+
 
 const app = express();
 
 app.use(express.json());
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
+
 app.use(router);
 
 app.listen(2222, () => console.log('Server is running'));
