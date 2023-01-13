614b5031d0db16ceb9bf7cd5ed26376e646d889b
pedroygor <pedroygor2010@hotmail.com>
2023-01-13 02:39:37 -0300

    feat: Adicionando migration CreateUsers

--- src/server.ts
+++ src/server.ts
@@ -12,8 +12,9 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
 app.use(router);
 
 app.listen(2222, () => console.log('Server is running'));
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerFile));
